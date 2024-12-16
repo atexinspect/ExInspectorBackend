02d640d566e7db947aba116fff4ddf9c536bc0ec
Atex Inspect <atexinspect@hotmail.com>
2024-12-16 15:18:03 +0000

    Middleware updates.

---
+++ ExInspectorBackend/Middleware/FormatAPI.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+
+namespace ExInspectorBackend.Middleware
+{
+    public static class FormatAPI
+    {
+        public static string FormatString(string stringToFormat)
+        {
+
+            byte xorConstant = 0x53;
+
+            byte[] data = Convert.FromBase64String(stringToFormat);
+            for (int i = 0; i < data.Length; i++)
+            {
+                data[i] = (byte)(data[i] ^ xorConstant);
+            }
+            string formatText = Encoding.UTF8.GetString(data);
+
+            return formatText;
+
+        }
+    }
+}
--- ExInspectorBackend/Program.cs
+++ ExInspectorBackend/Program.cs
@@ -1,46 +1,13 @@
-using ExInspectorBackend.Middleware;
 using ExInspectorBackend.Models;
+using ExInspectorBackend.Middleware;
 using Microsoft.AspNetCore.Datasync;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
-
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
 builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(connectionString));
-
 builder.Services.AddDatasyncControllers();
-
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c =>
-{
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Api Key Auth", Version = "v1" });
-    c.AddSecurityDefinition("ApiKey", new OpenApiSecurityScheme
-    {
-        Description = "ApiKey must appear in header",
-        Type = SecuritySchemeType.ApiKey,
-        Name = "XApiKey",
-        In = ParameterLocation.Header,
-        Scheme = "ApiKeyScheme"
-    });
-    var key = new OpenApiSecurityScheme()
-    {
-        Reference = new OpenApiReference
-        {
-            Type = ReferenceType.SecurityScheme,
-            Id = "ApiKey"
-        },
-        In = ParameterLocation.Header
-    };
-    var requirement = new OpenApiSecurityRequirement
-    {
-        { key, new List<string>() }
-    };
-    c.AddSecurityRequirement(requirement);
-});
 
 var app = builder.Build();
 
@@ -51,16 +18,10 @@ using (var scope = app.Services.CreateScope())
     await context.InitializeDatabaseAsync().ConfigureAwait(false);
 }
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
 app.UseAuthorization();
-app.UseMiddleware<ApiKeyMiddleware>();
-
+app.UseMiddleware<Middleware>();
 app.UseHttpsRedirection();
-app.MapControllers(); // Added 08/12/23 after getting DataSyncAPITest app working locally.
+
+// Configure and run the web service.
+app.MapControllers();
 app.Run();
