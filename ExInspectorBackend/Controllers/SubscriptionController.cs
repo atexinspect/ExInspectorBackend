30ea16bfbfbae130682518b8256cff6db66a59bc
Atex Inspect <atexinspect@hotmail.com>
2025-12-12 21:57:49 +0000

    Add Subscription entity, controller, and migration
    Introduced a new Subscription model with relevant properties and integrated it into the EF Core context. Added a SubscriptionController for API access with soft delete enabled. Created a migration to add the Subscription table with data sync trigger, and updated the model snapshot accordingly.

---
+++ ExInspectorBackend/Controllers/SubscriptionController.cs
@@ -0,0 +1,17 @@
+﻿using CommunityToolkit.Datasync.Server;
+using CommunityToolkit.Datasync.Server.EntityFrameworkCore;
+using ExInspectorBackend.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExInspectorBackend.Controllers
+{
+    [Route("tables/subscription")]
+    public class SubscriptionController : TableController<Subscription>
+    {
+        public SubscriptionController(AppDbContext context)
+            : base(new EntityTableRepository<Subscription>(context))
+        {
+            Options = new TableControllerOptions { EnableSoftDelete = true };
+        }
+    }
+}
